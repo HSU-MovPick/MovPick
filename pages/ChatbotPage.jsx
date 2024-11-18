6c1e27baf5ed3126720af91cbc23168989ec855e
khyaejin <olivia019182@naver.com>
2024-11-18 20:27:43 +0900

    ✨ FEAT. MovieCard에 title, category, movieReleaseDate 추가

--- pages/ChatbotPage.jsx
+++ pages/ChatbotPage.jsx
@@ -1,11 +1,28 @@
 import React from 'react';
 import styled from 'styled-components/native';
 import Background from '../shared/components/StandardBackground';
+import MovieCard from '../shared/components/MovieCard';
 
-// export default function ChatbotPage(){
-//     return (
+export default function ChatbotPage() {
+// 데이터 전달 필요 -> 우선은 값 직접 전달
+  const movieTitle = "베테랑2";
+  const movieCategory = "범죄, 액션, 스릴러";
+  const movieReleaseDate = "2024.01.15";
 
+  return (
+    <Background>
+      <Container>
+        <MovieCard 
+          movieTitle={movieTitle} //제목
+          movieCategory={movieCategory}  //카테고리
+          movieReleaseDate={movieReleaseDate} //개봉일자
+        />
+      </Container>
+    </Background>
+  );
+}
 
-//     )
-
-// }
+const Container = styled.View`
+  justify-content: center;
+  align-items: center;
+`;
--- shared/components/MovieCard.jsx
+++ shared/components/MovieCard.jsx
@@ -1,11 +1,39 @@
-import React, { useState } from 'react';
-import MovieCardSVG from '../../assets/movie-card.svg'; 
+import { Image, StyleSheet, Text } from 'react-native';
+import MovieCardPNG from '../../assets/movie-card.png';
 
-export default function MovieCard() {
-  return(
-    
-    <MovieCardSVG>
-    </MovieCardSVG>
-  
+export default function MovieCard({movieTitle, movieCategory, movieReleaseDate}) {
+
+  return (
+    <>
+    <Image source={MovieCardPNG} style={styles.image} />
+    <Text style={styles.title}> {movieTitle} </Text>
+    <Text style={styles.category}> {movieCategory} </Text>
+    <Text style={styles.releaseDate}> {movieReleaseDate} </Text>
+    </>
   );
 }
+
+const styles = StyleSheet.create({
+  image: {
+    position: relative, // 부모 요소로 설정
+    width: 300,
+    height: 400,
+    resizeMode: 'contain',
+    transform: [{ rotate: '3deg' }], 
+    marginLeft: 10, // 왼쪽 마진
+  },
+
+  title:{
+    position: absolute, //자식 요소로 설정
+    position: 'absolute',
+    top: 20, 
+    left: 10,
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: 'white',
+    textShadowColor: 'rgba(0, 0, 0, 0.8)', // 그림자
+    textShadowOffset: { width: 1, height: 1 },
+    textShadowRadius: 3,
+  }
+
+});
