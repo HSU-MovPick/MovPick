667cb95d1c18fec962a1c3cc46f2310b8ec18bbd
khyaejin <olivia019182@naver.com>
2024-11-18 21:35:39 +0900

    ✨ FEAT. MovieCard css 수정

--- App.js
+++ App.js
@@ -3,17 +3,19 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Test from './pages/TestingPage';
-
+import ChatbotPage from './pages/ChatbotPage';
 
 const Stack = createStackNavigator();
 
 function App() {
   return (
     <NavigationContainer>
-        <Stack.Navigator initialRouteName="Test"
+        <Stack.Navigator initialRouteName="ChatbotPage"
+        // <Stack.Navigator initialRouteName="Test"
         screenOptions={{ headerShown: false, animationEnabled: false }}>
           <Stack.Screen name="Test" component={Test} />
           {/* name = "Test-> 이게 컴포넌트의 주소" */}
+          <Stack.Screen name="ChatbotPage" component={ChatbotPage} />
         </Stack.Navigator>
     </NavigationContainer>
   );
