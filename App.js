85b0ef0e9d748a565dbc3f0b39c4d955896b19c7
khyaejin <olivia019182@naver.com>
2024-12-04 23:03:16 +0900

    ✨ ChatbotPage - 기본 page 세팅

--- App.js
+++ App.js
@@ -12,9 +12,7 @@ import Main from './pages/Main';
 import MovieList from './pages/MovieList';
 import MovieDetail from './pages/MovieDetail';
 import MapPage from './pages/MapPage';
-import MapTest from './pages/MapTest';
-import ChatbotPage from './pages/ChatbotPage';
-import FooterNavigationBar from './shared/components/FooterNavigationBar';
+import ChatbotWelcomePage from './pages/ChatbotWelcomePage';
 
 // Google Maps Wrapper는 React Native에서 사용할 수 없음
 
@@ -46,6 +44,7 @@ function App() {
   return (
     <NavigationContainer>
         <Stack.Navigator initialRouteName="Login"
+        /* <Stack.Navigator initialRouteName="MapPage" */
         screenOptions={{ headerShown: false, animationEnabled: false }}>
           <Stack.Screen name="RecommendMain" component={RecommendMain} />
           <Stack.Screen name="EmotionRecommendMain" component={EmotionRecommendMain} />
@@ -57,7 +56,6 @@ function App() {
           <Stack.Screen name="MovieDetail" component={MovieDetail} />
           <Stack.Screen name="Test" component={Test} />
           <Stack.Screen name="MapPage" component={MapPage} />
-          <Stack.Screen name="MapTest" component={MapTest} />
           <Stack.Screen name="ChatbotPage" component={ChatbotPage} />
         </Stack.Navigator>
     </NavigationContainer>
