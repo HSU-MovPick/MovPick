f32a0a985f8df706e15726f8a7560d4b16965885
Hjwoon <hjw030705@naver.com>
2024-12-06 04:49:03 +0900

    맵 페이지 디자인 추가

--- pages/MapPage.jsx
+++ pages/MapPage.jsx
@@ -1,11 +1,12 @@
 import React, { useEffect, useState, useRef } from 'react';
-import { View, StyleSheet, ActivityIndicator, Text, Image, Modal, TouchableOpacity, FlatList } from 'react-native';
+import { View, StyleSheet, ActivityIndicator, Text, Image, Modal, TouchableOpacity, FlatList, ImageBackground } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
 import * as Location from 'expo-location';
 import axios from 'axios';
 import { GOOGLE_API_KEY } from '../config/keys'; // Google Maps API 키 파일
 import FooterNavigationBar from '../shared/components/FooterNavigationBar';
 import cinemaData from '../data/cinemaData';
+import Background from '../shared/components/StandardBackground';
 
 export default function MapPage() {
   const [location, setLocation] = useState(null); // 사용자 위치 상태
@@ -144,88 +145,96 @@ export default function MapPage() {
 
   return (
     <>
-      <View style={styles.container}>
-        <MapView
-          style={styles.map}
-          initialRegion={{
-            latitude: location.latitude,
-            longitude: location.longitude,
-            latitudeDelta: 0.06,
-            longitudeDelta: 0.06,
-          }}
-          showsUserLocation={true} // 현재 위치를 기본 마커로 표시
-        >
-          {/* 현재 위치 마커 */}
-          <Marker
-            coordinate={location}
-            title={address}
-            description={"저는 여기 있어요!"}
-            anchor={{ x: 0.5, y: 0.5 }} // 마커 기준점을 조정
+      <Background>
+        <View style={styles.container}>
+          {/* 지도 위에 배경 이미지 */}
+          <Image
+            source={require('../assets/map-bg.png')} // 로컬 PNG 이미지 경로
+            style={styles.mapCard}
+            resizeMode="contain"
+          />
+          <MapView
+            style={styles.map}
+            initialRegion={{
+              latitude: location.latitude,
+              longitude: location.longitude,
+              latitudeDelta: 0.06,
+              longitudeDelta: 0.06,
+            }}
+            showsUserLocation={true} // 현재 위치를 기본 마커로 표시
           >
-            <Image
-              source={require('../assets/mapicon.jpg')} // 로컬 이미지 경로
-              style={{
-                width: 40, // 너비 조정
-                height: 40, // 높이 조정
-                transform: [{ translateY: -25 }], // 이미지 상단으로 이동
-              }}
-              resizeMode="contain" // 이미지 비율 유지
-            />
-          </Marker>
-
-          {/* 영화관 마커 */}
-          {cinemas.map((cinema, index) => {
-            if (!cinema.geometry || !cinema.geometry.location) {
-              return null;
-            }
-            return (
-              <Marker
-                key={index}
-                coordinate={{
-                  latitude: cinema.geometry.location.lat,
-                  longitude: cinema.geometry.location.lng,
+            {/* 현재 위치 마커 */}
+            <Marker
+              coordinate={location}
+              title={address}
+              description={"저는 여기 있어요!"}
+              anchor={{ x: 0.5, y: 0.5 }} // 마커 기준점을 조정
+            >
+              <Image
+                source={require('../assets/mapicon.jpg')} // 로컬 이미지 경로
+                style={{
+                  width: 40, // 너비 조정
+                  height: 40, // 높이 조정
+                  transform: [{ translateY: -25 }], // 이미지 상단으로 이동
                 }}
-                title={cinema.name}
-                description={cinema.vicinity}
-                onPress={() => onMarkerPress(`cinema${index + 1}`)} // 마커 클릭 핸들러 연결
+                resizeMode="contain" // 이미지 비율 유지
               />
-            );
-          })}
-        </MapView>
-      </View>
-      <Modal
-        visible={modalVisible}
-        animationType="slide"
-        transparent={true}
-        onRequestClose={() => setModalVisible(false)}
-      >
-        <View style={styles.modalContainer}>
-          <Text style={styles.modalTitle}>Movies Playing</Text>
-          {selectedCinema && selectedCinema.length > 0 ? (
-            <FlatList
-              data={selectedCinema}
-              keyExtractor={(item) => item.id}
-              renderItem={({ item }) => (
-                <View style={styles.movieItem}>
-                  <Text style={styles.movieTitle}>{item.title}</Text>
-                  <Text style={styles.showtimes}>
-                    {item.showtimes.join(", ")}
-                  </Text>
-                </View>
-              )}
-            />
-          ) : (
-            <Text>No movies available for this cinema.</Text>
-          )}
+            </Marker>
 
-          <TouchableOpacity
-            style={styles.closeButton}
-            onPress={() => setModalVisible(false)}
-          >
-            <Text style={styles.closeButtonText}>Close</Text>
-          </TouchableOpacity>
+            {/* 영화관 마커 */}
+            {cinemas.map((cinema, index) => {
+              if (!cinema.geometry || !cinema.geometry.location) {
+                return null;
+              }
+              return (
+                <Marker
+                  key={index}
+                  coordinate={{
+                    latitude: cinema.geometry.location.lat,
+                    longitude: cinema.geometry.location.lng,
+                  }}
+                  title={cinema.name}
+                  description={cinema.vicinity}
+                  onPress={() => onMarkerPress(`cinema${index + 1}`)} // 마커 클릭 핸들러 연결
+                />
+              );
+            })}
+          </MapView>
         </View>
-      </Modal>
+        <Modal
+          visible={modalVisible}
+          animationType="slide"
+          transparent={true}
+          onRequestClose={() => setModalVisible(false)}
+        >
+          <View style={styles.modalContainer}>
+            <Text style={styles.modalTitle}>Movies Playing</Text>
+            {selectedCinema && selectedCinema.length > 0 ? (
+              <FlatList
+                data={selectedCinema}
+                keyExtractor={(item) => item.id}
+                renderItem={({ item }) => (
+                  <View style={styles.movieItem}>
+                    <Text style={styles.movieTitle}>{item.title}</Text>
+                    <Text style={styles.showtimes}>
+                      {item.showtimes.join(", ")}
+                    </Text>
+                  </View>
+                )}
+              />
+            ) : (
+              <Text>No movies available for this cinema.</Text>
+            )}
+
+            <TouchableOpacity
+              style={styles.closeButton}
+              onPress={() => setModalVisible(false)}
+            >
+              <Text style={styles.closeButtonText}>Close</Text>
+            </TouchableOpacity>
+          </View>
+        </Modal>
+      </Background>
 
       {/* 하단 네비게이션 바 푸터 */}
       <FooterNavigationBar />
@@ -235,10 +244,23 @@ export default function MapPage() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 1, // 화면 전체를 차지하도록 설정
+    justifyContent: 'center', // 세로 방향 중앙 정렬
+    alignItems: 'center', // 가로 방향 중앙 정렬
+    paddingTop: 50, // 상단에서 50px 띄우기
   },
   map: {
-    ...StyleSheet.absoluteFillObject, // 지도 크기를 화면 전체로 설정
+    position: 'absolute', // 지도 위치를 고정
+    ...StyleSheet.absoluteFillObject, // 지도는 화면 전체를 채움
+    zIndex: 3, // 이미지 위에 지도 표시
+    top: 100, // 상단에서 100px 떨어짐
+    left: -135, // 왼쪽에서 45px 띄움
+    right: 20, // 오른쪽에서 20px 띄움
+    height: 520, // 지도 높이
+    width: 270, // 지도 너비
+    borderWidth: 0, // 테두리 추가
+    borderColor: '#000', // 테두리 색상
+    borderRadius: 25, // 테두리에 둥근 모서리 효과 추가
   },
   loadingContainer: {
     flex: 1,
@@ -292,5 +314,13 @@ const styles = StyleSheet.create({
   closeButtonText: {
     color: "#fff",
     fontWeight: "bold",
+  },
+  mapCard: {
+    position: 'absolute', // 배경 이미지를 절대 위치로 설정
+    top: 50, // 지도 위 50px 위치
+    alignSelf: 'center', // 화면 가로 중심 정렬
+    width: 1500, // 원하는 너비 설정
+    height: 680, // 원하는 높이 설정
+    zIndex: 2 // 배경 이미지는 기본 계층
   }
 });
