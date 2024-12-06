1423a848fc501b2feaf38eff40dddd68bd8a8f52
frombunny <rucy011121@gmail.com>
2024-12-06 15:29:12 +0900

    영화 예고편 페이지 추가

--- pages/VideoPage.jsx
+++ pages/VideoPage.jsx
@@ -1,17 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components/native';
 import Background from '../shared/components/StandardBackground';
-import { Image, ScrollView, FlatList, TouchableOpacity, Text, ActivityIndicator } from 'react-native';
+import { Image, FlatList, TouchableOpacity, Text, ActivityIndicator } from 'react-native';
 import RecommendMainButton from '../entities/RecommendMain/ui/RecommendMainButton';
 import { useNavigation, Linking } from '@react-navigation/native';
 import FooterNavigationBar from '../shared/components/FooterNavigationBar';
 import axios from 'axios';
-import { GOOGLE_API_KEY } from '../config/keys';
+import { GOOGLE_CLOUD_API_KEY } from '../config/google-cloud-api-key';
+const YOUTUBE_API_KEY = GOOGLE_CLOUD_API_KEY;
+const MOVIE_TITLE = "영화 예고편"; // 검색할 영화 제목
 
-const YOUTUBE_API_KEY = GOOGLE_API_KEY;
-const MOVIE_TITLE = "Interstellar"; // 검색할 영화 제목
-
-// 영화 영상 페이지
 export default function VideoPage() {
   const navigation = useNavigation();
   const [videos, setVideos] = useState([]);
@@ -23,8 +21,11 @@ export default function VideoPage() {
 
   const fetchYouTubeVideos = async () => {
     try {
+      const query = encodeURIComponent(`${MOVIE_TITLE} trailer`);
+      console.log("API Key:", YOUTUBE_API_KEY);
+
       const response = await axios.get(
-        `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q=${MOVIE_TITLE} trailer&type=video&key=${YOUTUBE_API_KEY}`
+        `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q=${query}&type=video&key=${YOUTUBE_API_KEY}`
       );
       setVideos(response.data.items);
       setLoading(false);
@@ -59,48 +60,33 @@ export default function VideoPage() {
   return (
     <>
       <Background>
-        <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
-          <RecommendMainLayout>
-            <RecommnedFrame>
-              <Intro>영화 예고편을 확인해보세요!</Intro>
-              {loading ? (
-                <ActivityIndicator size="large" color="#FFFFFF" />
-              ) : (
-                <FlatList
-                  data={videos}
-                  renderItem={renderVideoItem}
-                  keyExtractor={(item) => item.id.videoId}
-                  contentContainerStyle={{ paddingBottom: 20 }}
-                />
-              )}
-              <ButtonWrapper>
-                <RecommendMainButton
-                  text="감정 분석하러 가기"
-                  onPress={() => navigation.navigate('EmotionRecommendMain')}
-                />
-              </ButtonWrapper>
-            </RecommnedFrame>
-          </RecommendMainLayout>
-        </ScrollView>
+        {loading ? (
+          <ActivityIndicator size="large" color="#FFFFFF" />
+        ) : (
+          <FlatList
+            data={videos}
+            renderItem={renderVideoItem}
+            keyExtractor={(item) => item.id.videoId}
+            contentContainerStyle={{ flexGrow: 1, paddingBottom: 20 }}
+            ListHeaderComponent={
+              <>
+                <Intro>최신 영화 예고편을 확인해보세요!</Intro>
+              </>
+            }
+          />
+        )}
       </Background>
       <FooterNavigationBar />
     </>
   );
 }
 
-const RecommendMainLayout = styled.View`
-  padding: 80px 3px;
-  gap: 60px;
-`;
-const RecommnedFrame = styled.View`
-  gap: 5px;
-`;
 const ButtonWrapper = styled.View`
   align-items: center;
   margin-top: 20px;
 `;
 const Intro = styled.Text`
-  font-size: 30px;
+  font-size: 20px;
   color: #FFFFFF;
   font-weight: 900;
   text-align: left;
